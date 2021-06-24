866837dfd540164de9be7d3029ba273c0d5df340
wlh <1056979087@qq.com>
2021-06-24 11:03:37 +0800

    dev开发啊

--- dataStructureAlgorithm/src/main/java/com/wlh/First.java
+++ dataStructureAlgorithm/src/main/java/com/wlh/First.java
@@ -2,4 +2,11 @@ package com.wlh;
 
 public class First {
     // dev 增加了第二个类
+    public static void main(String[] args) {
+
+        Integer i1 = 19;
+        Integer i2 = 20;
+
+        System.out.println(i1.equals(i2));
+    }
 }
