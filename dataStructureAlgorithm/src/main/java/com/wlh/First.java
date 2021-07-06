090754fd455f4db71c96c22ffa0e4b8419065553
wlh <1056979087@qq.com>
2021-07-06 17:08:32 +0800

    静态泛型

--- dataStructureAlgorithm/src/main/java/com/wlh/First.java
+++ dataStructureAlgorithm/src/main/java/com/wlh/First.java
@@ -2,4 +2,13 @@ package com.wlh;
 
 public class First {
     // dev 增加了第二个类
+
+    public static void main(String[] args) {
+        String a = "hello world";
+        First.find(a);
+    }
+
+    public static <T> Zero find(T an) {
+        return new Zero();
+    }
 }
